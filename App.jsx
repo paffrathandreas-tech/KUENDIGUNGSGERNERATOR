02c2359d51c4e9d6bafd168c299075a75810656a
paffrathandreas-tech <paffrath.andreas@googlemail.com>
2026-06-22 14:39:18 +0200

    Create App component with deployment message

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Kündigungsgenerator läuft ✅</h1>
+      <p>Deine App wurde erfolgreich deployed!</p>
+    </div>
+  );
+}
